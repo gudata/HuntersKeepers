a76e438695f5e6070d2f627fdcb8141d0c7400c2
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-02-16 12:53:19 -0500

    WIP

--- app/javascript/components/hunter_moves.vue
+++ app/javascript/components/hunter_moves.vue
@@ -1,14 +1,30 @@
 <template>
-  <ul>
-    <li v-for="move in moves"><strong>{{move.name}}:</strong> {{move.description}}</li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="move in moves"><input type="checkbox" :checked="move.has_move"><strong>{{move.name}}:</strong> {{move.description}}</li>
+    </ul>
+    <b-button @click="submitMoves">Submit Moves</b-button>
+  </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    selectedMoves: function() {
+      return this.moves.filter((move) => {
+        console.log(move.has_move);
+        return move.has_move;
+      });
+    }
+  },
   data: function () {
     return {
       moves: []
+    }
+  },
+  methods: {
+    submitMoves() {
+      put('/hunters/${hunter_id}', { 'moves_attributes': this.selectedMoves });
     }
   },
   mounted: function() {
